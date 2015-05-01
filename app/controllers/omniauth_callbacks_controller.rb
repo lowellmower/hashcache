dd063c70d4edd7f620e6cf0700d5c81c2bdef7ca
Lowell Mower <lowell.m.mower@gmail.com>
2015-05-01 11:31:29 -0400

    Update user model self from omni method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      # set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
     else
       session["devise.instagram_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
